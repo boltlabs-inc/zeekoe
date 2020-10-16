0edb12b76c5833a59b4c6d73958b7637dcba22fa
Kenny Foner <kwf@very.science>
2020-10-16 16:10:55 -0400

    Fix intermittent build failure by providing full path in protobuf module system

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/wire.proto")?;
+    tonic_build::configure().compile(&["proto/wire.proto"], &["proto/"])?;
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,14 @@ pub mod wire {
     tonic::include_proto!("wire");
     
     pub mod establish {
-        tonic::include_proto!("establish");
+        tonic::include_proto!("wire.establish");
     }
     
     pub mod activate {
-        tonic::include_proto!("activate");
+        tonic::include_proto!("wire.activate");
     }
 
     pub mod pay {
-        tonic::include_proto!("pay");
+        tonic::include_proto!("wire.pay");
     }
 }
