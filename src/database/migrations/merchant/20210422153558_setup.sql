dfe1efb2bbd12e82a8c6e23a935c19b7137fb69d
Mukund Lakshman <yaymukund@gmail.com>
2021-06-11 19:48:25 +0100

    Use the zkabacus types for `Revocation{Lock,Secret}`.

--- src/database/migrations/merchant/20210422153558_setup.sql
+++ src/database/migrations/merchant/20210422153558_setup.sql
@@ -6,7 +6,7 @@ CREATE UNIQUE INDEX nonces_data ON nonces (data);
 
 CREATE TABLE revocations (
   id SERIAL PRIMARY KEY,
-  lock VARCHAR(256) NOT NULL,
-  secret VARCHAR(256)
+  lock BLOB NOT NULL,
+  secret BLOB
 );
 CREATE INDEX revocations_lock on revocations (lock);
