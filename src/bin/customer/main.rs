a60a146c665db49b580e0c11c30870826192cc16
Kenny Foner <kwf@very.science>
2021-06-16 19:28:53 -0400

    Thread RNG through customer command interface at top level, expose SessionKey from `connect`

--- src/bin/customer/main.rs
+++ src/bin/customer/main.rs
@@ -1,5 +1,6 @@
 use {
     async_trait::async_trait,
+    rand::{rngs::StdRng, SeedableRng},
     std::{convert::identity, env, path::Path},
     structopt::StructOpt,
 };
@@ -7,7 +8,7 @@ use {
 use zeekoe::{
     customer::{
         cli::{self, Account::*, Customer::*},
-        client::ZkChannelAddress,
+        client::{SessionKey, ZkChannelAddress},
         defaults::config_path,
         Chan, Cli, Client, Config,
     },
@@ -25,26 +26,29 @@ mod pay;
 /// to start with a valid loaded configuration.
 #[async_trait]
 pub trait Command {
-    async fn run(self, config: Config) -> Result<(), anyhow::Error>;
+    async fn run(self, rng: StdRng, config: Config) -> Result<(), anyhow::Error>;
 }
 
 pub async fn main_with_cli(cli: Cli) -> Result<(), anyhow::Error> {
     let config_path = cli.config.ok_or_else(config_path).or_else(identity)?;
     let config = Config::load(&config_path);
 
+    // TODO: let this be made deterministic during testing
+    let rng = StdRng::from_entropy();
+
     match cli.customer {
         Configure(cli::Configure { .. }) => {
             drop(config);
             tokio::task::spawn_blocking(|| Ok(edit::edit_file(config_path)?)).await?
         }
-        Account(Import(import)) => import.run(config.await?).await,
-        Account(Remove(remove)) => remove.run(config.await?).await,
-        List(list) => list.run(config.await?).await,
-        Rename(rename) => rename.run(config.await?).await,
-        Establish(establish) => establish.run(config.await?).await,
-        Pay(pay) => pay.run(config.await?).await,
-        Refund(refund) => refund.run(config.await?).await,
-        Close(close) => close.run(config.await?).await,
+        Account(Import(import)) => import.run(rng, config.await?).await,
+        Account(Remove(remove)) => remove.run(rng, config.await?).await,
+        List(list) => list.run(rng, config.await?).await,
+        Rename(rename) => rename.run(rng, config.await?).await,
+        Establish(establish) => establish.run(rng, config.await?).await,
+        Pay(pay) => pay.run(rng, config.await?).await,
+        Refund(refund) => refund.run(rng, config.await?).await,
+        Close(close) => close.run(rng, config.await?).await,
     }
 }
 
@@ -52,7 +56,7 @@ pub async fn main_with_cli(cli: Cli) -> Result<(), anyhow::Error> {
 pub async fn connect(
     config: &Config,
     address: ZkChannelAddress,
-) -> Result<Chan<protocol::ZkChannels>, anyhow::Error> {
+) -> Result<(SessionKey, Chan<protocol::ZkChannels>), anyhow::Error> {
     let Config {
         backoff,
         connection_timeout,
