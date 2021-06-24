2ea7ae5490e1a36ed412d820544d68ab3e02483a
Marcella Hastings <marcellahastings@gmail.com>
2021-06-24 16:19:19 -0400

    Add mutual close session type #47
    Also fixes the build

--- src/bin/customer/close.rs
+++ src/bin/customer/close.rs
@@ -2,30 +2,47 @@ use {async_trait::async_trait, rand::rngs::StdRng};
 
 use zeekoe::customer::{cli::Close, Config};
 
-use super::Command;
+use super::{connect, database, Command};
+use anyhow::Context;
 
 #[async_trait]
 impl Command for Close {
     #[allow(unused)]
     async fn run(self, mut rng: StdRng, config: self::Config) -> Result<(), anyhow::Error> {
         if self.force {
-            unilateral_close()
+            unilateral_close(&self, rng, config)
                 .await
                 .context("Unilateral close failed.")?;
         } else {
-            mutual_close().await.context("Mutual close failed.")?;
+            mutual_close(&self, rng, config)
+                .await
+                .context("Mutual close failed.")?;
         }
         todo!()
     }
 }
 
-async fn unilateral_close() -> Result<(), Error> {
+async fn unilateral_close(close: &Close, mut rng: StdRng, config: self::Config) -> Result<(), anyhow::Error> {
     todo!()
 }
 
-async fn mutual_close() -> Result<(), Error> {
+async fn mutual_close(close: &Close, mut rng: StdRng, config: self::Config) -> Result<(), anyhow::Error> {
+    let database = database(&config)
+        .await
+        .context("Failed to connect to local database")?;
+
+    // Look up the address and current local customer state for this merchant in the database
+    let address = match database
+        .channel_address(&close.label)
+        .await
+        .context("Failed to look up channel address in local database")?
+    {
+        None => return Err(anyhow::anyhow!("Unknown channel label: {}", close.label)),
+        Some(address) => address,
+    };
+
     // Connect and select the Close session
-    let (session_key, chan) = connect(&config, address)
+    let (_session_key, chan) = connect(&config, &address)
         .await
         .context("Failed to connect to merchant")?;
 
