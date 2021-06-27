856694bc74088a49dc476624bd81263a7cb19451
Kenny Foner <kwf@very.science>
2021-06-27 10:52:06 -0400

    Addresses immediate error in #108 but does not fix API design issue

--- src/bin/customer/pay.rs
+++ src/bin/customer/pay.rs
@@ -196,15 +196,21 @@ async fn start_payment(
             })?;
 
             // Attempt to start the payment using the payment amount and proof context
-            let (started, start_message) = ready
-                .start(rng, payment_amount, &context)
-                .context("Failed to generate nonce and payment proof")?;
+            match ready.start(rng, payment_amount, &context) {
+                Ok((started, start_message)) => {
+                    // Set the new started state in the database
+                    *state = Some(State::Started(started));
 
-            // Set the new started state in the database
-            *state = Some(State::Started(started));
+                    // Return the start message
+                    Ok(start_message)
+                }
+                Err((ready, error)) => {
+                    // Put the old ready state back in the database
+                    *state = Some(State::Ready(ready));
 
-            // Return the start message
-            Ok(start_message)
+                    Err(error).context("Failed to generate nonce and pay proof")
+                }
+            }
         })
         .await
         .context("Database error while fetching initial pay state")??
