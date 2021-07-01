fddb8a397eb20d00bfe4c14d131bab8c78a9f2be
J. Ayo Akinyele <ayo@boltlabs.io>
2021-06-30 20:24:00 -0400

    updates to zkchannels_pytezos_mgr.py

--- zkchannel_pytezos_mgr.py
+++ zkchannel_pytezos_mgr.py
@@ -15,6 +15,9 @@ def read_json_file(json_file):
     f.close()
     return json.loads(s)
 
+def convert_mt_to_tez(balance):
+    return str(int(balance) /1000000)
+
 class FeeTracker:
     def __init__(self):
         self.fees = []
@@ -78,9 +81,9 @@ def originate(cust_py, init_params, cust_funding, merch_funding):
     print("Originate zkChannel ophash: ", out['hash'])
     # Get address of main zkchannel contract
     opg = pytezos.shell.blocks[-20:].find_operation(out['hash'])
-    main_id = opg['contents'][0]['metadata']['operation_result']['originated_contracts'][0]
+    contract_id = opg['contents'][0]['metadata']['operation_result']['originated_contracts'][0]
     print("zkChannel contract address: ", main_id)
-    return out, main_id
+    return out, contract_id
 
 def cust_close(ci, cust_close_data):
     # Form cust close storage
@@ -88,25 +91,20 @@ def cust_close(ci, cust_close_data):
     sigma1, sigma2 = cs.get("sigma1"), cs.get("sigma2")
     revocation_lock = cust_close_data.get("revocation_lock")
     # assumes it's already in the mutez
-    cust_balance = str(cust_close_data.get("customer_balance"))
-    merch_balance = str(cust_close_data.get("merchant_balance"))
+    cust_balance = convert_mt_to_tez(cust_close_data.get("customer_balance"))
+    merch_balance = convert_mt_to_tez(cust_close_data.get("merchant_balance"))
 
-    # close_storage = {
-    #     "custBal": cust_balance,
-    #     "merchBal": merch_balance,
-    #     "revLock": revocation_lock,
-    #     "s1": sigma1,
-    #     "s2": sigma2
-    # }
+    close_storage = {
+        "custBal": cust_balance,
+        "merchBal": merch_balance,
+        "revLock": revocation_lock,
+        "s1": sigma1,
+        "s2": sigma2
+    }
 
-    print("Broadcasting Cust Close")
-    #out = ci.custClose(close_storage).inject(_async=False)
-    #print("Cust Close ophash: ", out['hash'])
-    entrypoint = "custClose"
-    cmd = 'ci.{e}(\"{custBal}\", \"{merchBal}\", \"{revLock}\", \"{s1}\", \"{s2}\").inject(_async=False)'.format(e=entrypoint, 
-    custBal=cust_balance, merchBal=merch_balance, revLock=revocation_lock, s1=sigma1, s2=sigma2)
-    out = eval(cmd)
-    print("{} ophash: ".format(entrypoint), out['hash'])
+    print("Broadcasting Cust Close: %s" % close_storage)
+    out = ci.custClose(close_storage).inject(_async=False)
+    print("Cust Close ophash: ", out['hash'])
     return out
 
 def merch_dispute(ci, entrypoint, rev_secret):
@@ -143,7 +141,7 @@ def zkchannel_establish(feetracker, cust_py, merch_py, establish_params):
 
     return contract_id
 
-def zkchannel_unilateral_close(feetracker, contract_id, cust_py, merch_py, cust_close_data):
+def zkchannel_unilateral_close(feetracker, contract_id, cust_py, _merch_py, cust_close_data):
     '''
     Customer or Merchant can proceed with broadcasting 
     closing signature on final state of the channel.
@@ -160,109 +158,6 @@ def zkchannel_unilateral_close(feetracker, contract_id, cust_py, merch_py, cust_
     feetracker.add_result('custClaim', out)
 
     return 
-
-
-# def scenario1(feetracker, cust_py, merch_py, cust_close_json):
-#     '''
-#     Scenario 1: Customer creates a single funded contract then initiates a unilateral closure.
-#     Entrypoints tested: 'addFunding', 'custClose', 'custClaim'.
-#     '''
-#     print("Scenario 1: origination -> cust_funding -> cust_close -> cust_claim")
-
-#     # A single-funded channel is originated where the customer's initial balances (in mutez) is:
-#     cust_funding=30000000
-#     merch_funding=0
-#     out, main_id = originate(cust_py, cust_close_json, cust_funding, merch_funding)
-#     feetracker.add_result('originate', out) # feetracker is used to track fees for benchmarking purposes 
-
-#     # Set the contract interfaces for cust
-#     cust_ci = cust_py.contract(main_id)
-
-#     # add customer's balance to the contract using 'addFunding' entrypoint
-#     out = add_funding(cust_ci, cust_funding)
-#     feetracker.add_result('addFunding', out)
-
-#     # customer initates unilateral closure using 'custClose' entrypoint. 
-#     # The merchant's closing signature is included in 'cust_close_json'.
-#     out = cust_close(cust_ci, cust_close_json)
-#     feetracker.add_result('custClose', out)
-#     out = entrypoint_no_args(cust_ci, 'custClaim')
-#     feetracker.add_result('custClaim', out)
-
-# def scenario2(feetracker, cust_py, merch_py, cust_close_json):
-#     '''
-#     Scenario 2: First the merchant initiates a unilateral closure, then the customer closes on an outdated state and is punished by the merchant.
-#     Entrypoints tested: 'addFunding', 'expiry', 'custClose', 'merchDispute'.
-#     '''
-#     print("Scenario 2: origination -> cust_funding -> merch_funding -> expiry -> cust_close -> merch_dispute")
-#     # A dual-funded channel is being created where the initial balances (in mutez) will be:
-#     cust_funding=20000000
-#     merch_funding=10000000
-#     # The customer originates the custom zkchannel contract with initial storage arguments
-#     out, main_id = originate(cust_py, cust_close_json, cust_funding, merch_funding)
-
-#     # Set contract interfaces for cust and merch
-#     cust_ci = cust_py.contract(main_id)
-#     merch_ci = merch_py.contract(main_id)
-
-#     # customer funds their side of the contract with the same amount as their initial balance
-#     out = add_funding(cust_ci, cust_funding)
-
-#     # At this point, the customer sends the contract (KT1) address to the merchant.
-#     # After the customer's funding has reached 20 confirmations, the merchant funds their side of the channel.
-#     out = add_funding(merch_ci, merch_funding)
-
-#     # Merchant initiates closure by calling the 'expiry' entrypoint.
-#     out = entrypoint_no_args(merch_ci, 'expiry')
-
-#     # Customer initiates closure in response to 'expiry'.
-#     out = cust_close(cust_ci, cust_close_json)
-
-#     # Here, we simulate the customer closing on an old state. 
-#     # The merchant views the contract storage and checks if 'rev_lock' has been seen before. 
-#     # rev_lock has been seen, the merchant will punish the customer by calling 'merchDispute' and providing the revocation secret.
-#     # If the secret is valid, the contract will give the total balance to the merchant.
-#     rev_secret = add_hex_prefix(merch_close_json['rev_secret'])
-#     out = merch_dispute(merch_ci, 'merchDispute', rev_secret)
-#     feetracker.add_result('merchDispute', out)
-
-# def scenario3(feetracker, cust_py, merch_py, cust_close_json):
-#     '''
-#     Scenario 3: Tests the 'reclaimFunding' and 'merchClaim' entrypoints.
-#     'reclaimFunding' is to be used in a dual funded channel where only one party has deposited their funds and wishes to abort channel establishment.
-#     'expiry' is used by the merchant to force the customer to close the channel within the delay period.
-#     'merchClaim' is used by the merchant to claim the total channel balance if the customer fails to respond to the 'expiry' call.
-#     Entrypoints tested: 'addFunding', 'reclaimFunding', 'expiry', 'merchClaim'.
-#     '''
-#     print("Scenario 3: origination -> cust_funding -> reclaim_funding -> cust_funding -> merch_funding -> expiry -> merch_claim")
-
-#     cust_funding=20000000
-#     merch_funding=10000000
-#     out, main_id = originate(cust_py, cust_close_json, cust_funding, merch_funding)
-
-#     # Set pytezos contract interfaces for the cust and merch
-#     cust_ci = cust_py.contract(main_id)
-#     merch_ci = merch_py.contract(main_id)
-
-#     # customer funds their side of the contract with the same amount as their initial balance
-#     out = add_funding(cust_ci, cust_funding)
-
-#     # The customer reclaims their initial funding. 
-#     # This entrypoint is used in the situation where the merchant aborts channel establishment and the customer wants to get their funds back.
-#     out = entrypoint_no_args(cust_ci, 'reclaimFunding')
-#     feetracker.add_result('reclaimFunding', out)
-
-#     # customer and merchant fund their balances
-#     out = add_funding(cust_ci, cust_funding)
-#     out = add_funding(merch_ci, merch_funding)
-
-#     out = entrypoint_no_args(merch_ci, 'expiry')
-#     feetracker.add_result('expiry', out)
-
-#     # If the customer has not broadcasted 'custClose' in response to 'expiry', the merchant can claim the full channel balance with 'merchClaim'.
-#     out = entrypoint_no_args(merch_ci, 'merchClaim')
-#     feetracker.add_result('merchClaim', out)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -290,37 +185,38 @@ if __name__ == "__main__":
     cust_addr = read_json_file(cust_acc)['pkh']
     merch_py = pytezos.using(key=merch_acc)
     merch_addr = read_json_file(merch_acc)['pkh']
-
     # merch_close_json = read_json_file(merch_close_file)
 
     # load zchannel contracts
     main_code = ContractInterface.from_file(args.contract)
 
     # Activate cust and merch testnet accounts
-    try:
-        print("Activating cust account")
-        cust_py.activate_account().fill().sign().inject()
-    except:
-        print("Cust account already activated")
+    if args.establish:
+        try:
+            print("Activating cust account")
+            cust_py.activate_account().fill().sign().inject()
+        except:
+            print("Cust account already activated")
 
-    try:
-        print("Revealing cust pubkey")
-        out = cust_py.reveal().autofill().sign().inject()
-    except:
-        pass
+        try:
+            print("Revealing cust pubkey")
+            out = cust_py.reveal().autofill().sign().inject()
+        except:
+            pass
     cust_pubkey = cust_py.key.public_key()
 
-    try:
-        print("Activating merch account")
-        merch_py.activate_account().fill().sign().inject()
-    except: 
-        print("Merch account already activated")
+    if args.establish:
+        try:
+            print("Activating merch account")
+            merch_py.activate_account().fill().sign().inject()
+        except: 
+            print("Merch account already activated")
 
-    try:
-        print("Revealing merch pubkey")
-        out = merch_py.reveal().autofill().sign().inject()
-    except:
-        pass
+        try:
+            print("Revealing merch pubkey")
+            out = merch_py.reveal().autofill().sign().inject()
+        except:
+            pass
     merch_pubkey = merch_py.key.public_key()
 
     feetracker = FeeTracker()
