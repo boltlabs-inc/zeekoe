8274dff606bd7c6e2fd93475f3f52ff60e50bbe8
Kenny Foner <kwf@very.science>
2021-08-23 16:34:47 -0400

    Begin pytezos integration, using inline-python

--- src/escrow/tezos.rs
+++ src/escrow/tezos.rs
@@ -1,8 +1,90 @@
-mod establish {
-    use crate::escrow::{
-        notify::{Level, Notifications},
-        types::*,
+use pyo3::types::PyDict;
+
+/// The Michelson contract code for the ZkChannels contract.
+static CONTRACT_CODE: &str = todo!(); //include_str!("tezos_contract.tz");
+
+/// The ZkChannels close scalar as bytes
+static CLOSE_SCALAR: &'static [u8] = &zkabacus_crypto::CLOSE_SCALAR.to_bytes();
+
+lazy_static::lazy_static! {
+    /// Close scalar bytes as a byte string
+
+    /// The python execution context used for all pytezos operations.
+    static ref PYTHON_CONTEXT: inline_python::Context = inline_python::python! {
+        from pytezos import pytezos, Contract, ContractInterface
+        import json
+
+        main_code = ContractInterface.from_michelson('CONTRACT_CODE)
+
+        close_scalar_bytes = 'CLOSE_SCALAR
+
+        def originate(cust_acc, channel_id,
+            merch_g2, merch_y2s, merch_x2,
+            cust_funding, merch_funding):
+            // Customer pytezos interface
+            cust_py = pytezos.using(key=cust_acc)
+
+            // Merchant's PS pubkey, used for verifying the merchant's signature in custClose.
+            g2 = merch_ps_pk.get("g2")
+            y2s = merch_ps_pk.get("y2s")
+            x2 = merch_ps_pk.get("x2")
+
+            initial_storage = {
+                "cid": channel_id,
+                "close_flag": close_scalar_bytes,
+                "context_string": "zkChannels mutual close",
+                "custAddr": cust_addr,
+                "custBal":0,
+                "custFunding": cust_funding,
+                "custPk": cust_pubkey,
+                "delayExpiry": "1970-01-01T00:00:00Z",
+                "g2":g2,
+                "merchAddr": merch_addr,
+                "merchBal": 0,
+                "merchFunding": merch_funding,
+                "merchPk": merch_pubkey,
+                "merchPk0": y2s[0],
+                "merchPk1": y2s[1],
+                "merchPk2": y2s[2],
+                "merchPk3": y2s[3],
+                "merchPk4": y2s[4],
+                "merchPk5": x2,
+                "revLock": "0x00",
+                "selfDelay": 3,
+                "status": 0
+            }
+
+            // Originate main zkchannel contract
+            out = cust_py.origination(script=main_code.script(initial_storage=initial_storage)).autofill().sign().send(min_confirmations=1)
+
+            // Get address of main zkchannel contract
+            opg = pytezos.shell.blocks[-20:].find_operation(out.hash())
+            main_id = opg["contents"][0]["metadata"]["operation_result"]["originated_contracts"][0]
+
+            return main_id
     };
+}
+
+fn merch_public_key_to_python_input(
+    public_key: &zkabacus_crypto::PublicKey,
+) -> (Vec<u8>, Vec<Vec<u8>>, Vec<u8>) {
+    let zkabacus_crypto::PublicKey { g2, y2s, x2, .. } = public_key;
+    let g2 = g2.to_compressed().to_vec();
+    let y2s = y2s
+        .into_iter()
+        .map(|y2| y2.to_compressed().to_vec())
+        .collect::<Vec<_>>();
+    let x2 = x2.to_compressed().to_vec();
+
+    (g2, y2s, x2)
+}
+
+mod establish {
+    use super::PYTHON_CONTEXT;
+    use crate::escrow::{notify::Level, types::*};
+    use inline_python::{python, Context};
+    use std::convert::TryFrom;
+    use tezedge::{FromBase58Check, OriginatedAddress, ToBase58Check};
     use zkabacus_crypto::{ChannelId, CustomerBalance, MerchantBalance, PublicKey};
 
     #[allow(unused)]
@@ -44,13 +126,35 @@ mod establish {
     /// the one in the provided [`CustomerFundingInformation`].
     #[allow(unused)]
     pub async fn originate(
-        notifications: &Notifications,
         merchant_funding_info: &MerchantFundingInformation,
         customer_funding_info: &CustomerFundingInformation,
         merchant_public_key: &PublicKey,
         originator_key_pair: &TezosKeyPair,
         channel_id: &ChannelId,
     ) -> Result<(ContractId, Level), Error> {
+        let (g2, y2s, x2) = super::merch_public_key_to_python_input(merchant_public_key);
+        let merchant_funding = merchant_funding_info.balance.into_inner();
+        let customer_funding = customer_funding_info.balance.into_inner();
+        let customer_account = customer_funding_info.address.to_base58check();
+        let channel_id = channel_id.to_bytes().to_vec();
+
+        PYTHON_CONTEXT.run(python! {
+            out = originate(
+                'customer_account, 'channel_id,
+                'g2, 'y2s, 'x2,
+                'customer_funding, 'merchant_funding
+            )
+        });
+        let out = PYTHON_CONTEXT.get::<String>("out");
+        PYTHON_CONTEXT.set("out", ());
+
+        let contract_id = ContractId::new(
+            OriginatedAddress::from_base58check(&out)
+                .expect("Contract id returned from pytezos must be valid base58"),
+        );
+
+        // TODO: handle errors submitting contract to chain (return error from call to python)
+
         todo!()
     }
 
@@ -66,7 +170,6 @@ mod establish {
     /// - the `addFunding` entrypoint has not been called by the customer address before.
     #[allow(unused)]
     pub async fn add_customer_funding(
-        notifications: &Notifications,
         contract_id: &ContractId,
         customer_funding_info: &CustomerFundingInformation,
         customer_key_pair: &TezosKeyPair,
@@ -89,7 +192,6 @@ mod establish {
     /// zkChannels contract or it does not have the expected storage.
     #[allow(unused)]
     pub async fn verify_origination(
-        notifications: &Notifications,
         contract_id: &ContractId,
         merchant_funding_info: &MerchantFundingInformation,
         customer_funding_info: &CustomerFundingInformation,
@@ -99,7 +201,7 @@ mod establish {
         todo!()
     }
 
-    /// Verify that the customer has sucessfully funded the contract via the `addFunding`
+    /// Verify that the customer has successfully funded the contract via the `addFunding`
     /// entrypoint
     ///
     /// Correct funding requires that:
@@ -110,7 +212,6 @@ mod establish {
     /// and is called by the merchant.
     #[allow(unused)]
     pub async fn verify_customer_funding(
-        notifications: &Notifications,
         contract_id: &ContractId,
         customer_funding_info: &CustomerFundingInformation,
     ) -> Result<(), Error> {
@@ -136,7 +237,6 @@ mod establish {
     /// - the contract status is not OPEN
     #[allow(unused)]
     pub async fn add_merchant_funding(
-        notifications: &Notifications,
         contract_id: &ContractId,
         merchant_funding_info: &MerchantFundingInformation,
         merchant_key_pair: &TezosKeyPair,
@@ -154,7 +254,6 @@ mod establish {
     /// - the `addFunding` entrypoint has not been called by the customer address
     #[allow(unused)]
     pub async fn reclaim_customer_funding(
-        notifications: &Notifications,
         contract_id: &ContractId,
         customer_key_pair: &TezosKeyPair,
     ) -> Result<(), Error> {
@@ -163,7 +262,7 @@ mod establish {
 }
 
 mod close {
-    use crate::escrow::{notify::Notifications, types::*};
+    use crate::escrow::types::*;
 
     use {
         tezedge::signer::OperationSignatureInfo,
@@ -184,7 +283,6 @@ mod close {
     ///   the specified contract
     #[allow(unused)]
     pub async fn expiry(
-        notifications: &Notifications,
         contract_id: &ContractId,
         merchant_key_pair: &TezosKeyPair,
     ) -> Result<(), Error> {
@@ -204,7 +302,6 @@ mod close {
     ///   contract
     #[allow(unused)]
     pub async fn merch_claim(
-        notifications: &Notifications,
         contract_id: &ContractId,
         merchant_key_pair: &TezosKeyPair,
     ) -> Result<(), Error> {
@@ -226,7 +323,6 @@ mod close {
     ///   public key on the expected tuple
     #[allow(unused)]
     pub async fn cust_close(
-        notifications: &Notifications,
         contract_id: &ContractId,
         close_message: &ClosingMessage,
         customer_key_pair: &TezosKeyPair,
@@ -246,7 +342,6 @@ mod close {
     /// - the [`RevocationSecret`] does not hash to the `rev_lock` field in the specified contract
     #[allow(unused)]
     pub async fn merch_dispute(
-        notifications: &Notifications,
         contract_id: &ContractId,
         revocation_secret: &RevocationSecret,
         merchant_key_pair: &TezosKeyPair,
@@ -266,7 +361,6 @@ mod close {
     /// - the [`TezosKeyPair`] does not match the `cust_addr` field in the specified contract
     #[allow(unused)]
     pub async fn cust_claim(
-        notifications: &Notifications,
         contract_id: &ContractId,
         customer_key_pair: &TezosKeyPair,
     ) -> Result<(), Error> {
@@ -280,7 +374,6 @@ mod close {
     /// This is called by the merchant.
     #[allow(unused)]
     pub async fn authorize_mutual_close(
-        notifications: &Notifications,
         contract_id: &ContractId,
         close_state: &CloseState,
         merchant_key_pair: &TezosKeyPair,
@@ -301,7 +394,6 @@ mod close {
     ///   on the expected tuple
     #[allow(unused)]
     pub async fn mutual_close(
-        notifications: &Notifications,
         contract_id: &ContractId,
         customer_balance: &CustomerBalance,
         merchant_balance: &MerchantBalance,
