991f92173cb0515881d5c216fccb17accff89809
Gijs Van Laer <gijs.vanlaer@gmail.com>
2021-10-29 14:55:00 +0200

    Add commands #288

--- .github/actions/e2e-action/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-pip install requests
-python3 test-zeekoe.py merch-setup --url "http://localhost:20000" -v
-python3 test-zeekoe.py cust-setup --url "http://localhost:20000" -v
-python3 test-zeekoe.py scenario --channel 1 -v --command-list establish pay pay pay_all close
-python3 test-zeekoe.py scenario --channel 1 -v --command-list establish pay store pay restore close
