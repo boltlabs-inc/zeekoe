081b75897bde92904a6fd059dab00ef10c7bb5b6
Marcella Hastings <marcellahastings@gmail.com>
2022-01-28 11:42:09 -0500

    call actual server processes #69

--- integration_tests/common.rs
+++ integration_tests/common.rs
@@ -5,15 +5,20 @@ use std::{
     sync::Mutex,
 };
 
-use futures::future::Join;
-use tokio::task::JoinHandle;
-
 use {
-    futures::future,
+    futures::future::{self, Join},
     thiserror::Error,
-    tracing::{error, info, info_span},
+    tokio::task::JoinHandle,
+    tracing::{error, info_span},
     tracing_futures::Instrument,
-    zeekoe::timeout::WithTimeout,
+};
+
+use rand::SeedableRng;
+use structopt::StructOpt;
+use zeekoe::{
+    customer::{cli::Customer as CustomerCli, zkchannels::Command},
+    merchant::{cli::Merchant as MerchantCli, zkchannels::Command as _},
+    timeout::WithTimeout,
 };
 
 pub const CUSTOMER_CONFIG: &str = "TestCustomer.toml";
@@ -21,7 +26,8 @@ pub const MERCHANT_CONFIG: &str = "TestMerchant.toml";
 pub const ERROR_FILENAME: &str = "errors.log";
 
 // Give a name to the slightly annoying type of the joined server futures
-type ServerFuture = Join<JoinHandle<()>, JoinHandle<()>>;
+type ServerFuture =
+    Join<JoinHandle<Result<(), anyhow::Error>>, JoinHandle<Result<(), anyhow::Error>>>;
 
 #[derive(Debug)]
 #[allow(unused)]
@@ -42,39 +48,44 @@ impl Party {
 
 pub async fn setup() -> ServerFuture {
     // write config options for each party
-    write_config_file(CUSTOMER_CONFIG, customer_test_config());
-    write_config_file(MERCHANT_CONFIG, merchant_test_config());
+    let customer_config = customer_test_config().await;
+    let merchant_config = merchant_test_config().await;
 
     tracing_subscriber::fmt()
         .with_writer(Mutex::new(
             File::create(ERROR_FILENAME).expect("Failed to open log file"),
         ))
         .init();
-    info!("spawning tasks now...");
 
-    // Stand-in task for the customer watcher.
-    let one = tokio::spawn(
-        async {
-            loop {
-                error!("This task worked exactly like I expected it to");
-                tokio::time::sleep(tokio::time::Duration::new(3, 0)).await;
-            }
-        }
-        .instrument(info_span!("customer watcher")),
+    // Form the customer watch request (cannot construct it directly because `Watch` is
+    // non-exhaustive)
+    let watch = match CustomerCli::from_iter(vec!["./target/debug/zkchannel-customer", "watch"]) {
+        CustomerCli::Watch(watch) => watch,
+        _ => panic!("Failed to parse customer watch CLI"),
+    };
+
+    // TODO: make this a fixed seed?
+    let rng = rand::rngs::StdRng::from_entropy();
+
+    let customer_handle = tokio::spawn(
+        watch
+            .run(rng, customer_config)
+            .instrument(info_span!(Party::CustomerWatcher.to_string())),
     );
+
+    // Form the merchant run request (same non-exhaustive situation here)
+    let run = match MerchantCli::from_iter(vec!["./target/debug/zkchannel-merchant", "run"]) {
+        MerchantCli::Run(run) => run,
+        _ => panic!("Failed to parse merchant run CLI"),
+    };
 
     // Stand-in task for the merchant server
-    let two = tokio::spawn(
-        async {
-            loop {
-                error!("This task is also good at its job");
-                tokio::time::sleep(tokio::time::Duration::new(5, 0)).await;
-            }
-        }
-        .instrument(info_span!("merchant server")),
+    let merchant_handle = tokio::spawn(
+        run.run(merchant_config)
+            .instrument(info_span!(Party::MerchantServer.to_string())),
     );
 
-    future::join(one, two)
+    future::join(customer_handle, merchant_handle)
 }
 
 pub async fn teardown(server_future: ServerFuture) {
@@ -85,7 +96,7 @@ pub async fn teardown(server_future: ServerFuture) {
 }
 
 /// Encode the customizable fields of the zeekoe customer Config struct for testing.
-fn customer_test_config() -> String {
+async fn customer_test_config() -> zeekoe::customer::Config {
     let m = HashMap::from([
         ("database", "{ sqlite = \"customer-sandbox.db\" }"),
         ("trust_certificate", "\"localhost.crt\""),
@@ -95,17 +106,30 @@ fn customer_test_config() -> String {
         ("confirmation_depth", "1"),
     ]);
 
-    m.into_iter().fold("".to_string(), |acc, (key, value)| {
+    let contents = m.into_iter().fold("".to_string(), |acc, (key, value)| {
         format!("{}{} = {}\n", acc, key.to_string(), value.to_string())
-    })
+    });
+
+    write_config_file(CUSTOMER_CONFIG, contents);
+
+    zeekoe::customer::Config::load(CUSTOMER_CONFIG)
+        .await
+        .expect("Failed to load customer config")
 }
 
 /// Encode the customizable fields of the zeekoe merchant Config struct for testing.
-fn merchant_test_config() -> String {
-    let tezos_config = customer_test_config()
-        .replace("alice", "bob")
-        .replace("customer", "merchant")
-        .replace("trust_certificate = \"localhost.crt\"\n", "");
+async fn merchant_test_config() -> zeekoe::merchant::Config {
+    let m = HashMap::from([
+        ("database", "{ sqlite = \"merchant-sandbox.db\" }"),
+        ("tezos_account", "{ alias = \"bob\" }"),
+        ("tezos_uri", "\"http://localhost:20000\""),
+        ("self_delay", "120"),
+        ("confirmation_depth", "1"),
+    ]);
+
+    let tezos_contents = m.into_iter().fold("".to_string(), |acc, (key, value)| {
+        format!("{}{} = {}\n", acc, key.to_string(), value.to_string())
+    });
 
     // helper to write out the service for ipv4 and v6 localhost addresses
     let generate_service = |addr: &str| {
@@ -120,12 +144,18 @@ fn merchant_test_config() -> String {
         })
     };
 
-    format!(
+    let contents = format!(
         "{}{}\n{}",
-        tezos_config,
+        tezos_contents,
         generate_service("::1"),
         generate_service("127.0.0.1")
-    )
+    );
+
+    write_config_file(MERCHANT_CONFIG, contents);
+
+    zeekoe::merchant::Config::load(MERCHANT_CONFIG)
+        .await
+        .expect("failed to load merchant config")
 }
 
 /// Write out the configuration in `contents` to the file at `path`.
@@ -141,6 +171,7 @@ fn write_config_file(path: &str, contents: String) {
 }
 
 #[derive(Debug, Error)]
+#[allow(unused)]
 pub enum LogError {
     #[error("Failed to open log file: {0}")]
     OpenFailed(std::io::Error),
@@ -153,6 +184,7 @@ pub enum LogError {
 /// Current behavior: outputs the entire log
 /// Ideal behavior: pass a Party, maybe an indicator of which test / channel name we want. Return
 /// only the lines relevant to that setting.
+#[allow(unused)]
 pub fn get_error() -> Result<String, LogError> {
     let mut file = File::open(ERROR_FILENAME).map_err(LogError::OpenFailed)?;
     let mut error = String::new();
--- integration_tests/main.rs
+++ integration_tests/main.rs
@@ -8,11 +8,10 @@ pub async fn main() {
 
     // Run tests
     let tests = &[failing_test, another_failing_test];
-    setup_works().await;
 
     // Run every test, printing out details if it fails
     let results = tests
-        .into_iter()
+        .iter()
         .map(|test| {
             let result = panic::catch_unwind(|| test());
             if result.is_err() {
@@ -27,26 +26,6 @@ pub async fn main() {
     assert!(results.iter().any(|result| result.is_err()));
 
     common::teardown(server_future).await;
-}
-
-async fn setup_works() {
-    // Make sure that the config files were encoded validly
-    let _customer_config = zeekoe::customer::Config::load(common::CUSTOMER_CONFIG)
-        .await
-        .expect("failed to load customer config");
-
-    let _merch_config = zeekoe::merchant::Config::load(common::MERCHANT_CONFIG)
-        .await
-        .expect("failed to load merchant config");
-
-    // Make sure that errors are written to the error log
-    match common::get_error() {
-        Ok(string) => {
-            println!("{}", string);
-            assert!(string.contains("This task is also good"))
-        }
-        Err(_) => assert!(false),
-    }
 }
 
 fn failing_test() {
